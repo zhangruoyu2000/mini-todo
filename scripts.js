5329b228a84e3b7a669fc27db5acdbf8755b4ab7
Your Name <you@example.com>
2020-06-23 14:50:20 +0800

    初步完成项目

--- scripts.js
+++ scripts.js
@@ -8,6 +8,10 @@ function renderEditor() {
 
 
     let addTask = () => {
+        if (inputEl.value.length === 0) {
+            return;//可增加警告
+
+        }
         let newTask = {
             title: inputEl.value,
             done: false,
@@ -48,25 +52,72 @@ function renderTaskItems() {
     for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
         let itemEl = document.createElement("div");
+        itemEl.className = "task";
 
         let doneEl = document.createElement("input");
         doneEl.type = "checkbox";
+        doneEl.checked = task.done;
+        if (task.done) {
+            itemEl.classList.add("done");
+        } else {
+            itemEl.classList.remove("done")
+        }
+        doneEl.onchange = (e) => {
+            task.done = e.target.checked;
+            if (task.done) {
+                itemEl.classList.add("done");
+            } else {
+                itemEl.classList.remove("done")
+            }
+        }
         itemEl.append(doneEl);
 
         let titleEl = document.createElement("label");
         titleEl.innerText = task.title;
         itemEl.append(titleEl);
 
-        let cancelEl = document.createElement("button");
-        cancelEl.innerText = "x";
-        cancelEl.onclick = () => {
-            tasks.splice(i, 1);
-            renderTaskItems();
-        }
-        itemEl.append(cancelEl);
+        let ctrlbarEl = renderTaskCtrlBar(tasks,i);
+
+        itemEl.append(ctrlbarEl);
 
         itemsEl.append(itemEl);
     }
+}
+
+function renderTaskCtrlBar(tasks,taskIdx) {
+    let ctrlbarEl = document.createElement("div");
+    ctrlbarEl.className = "ctrlbar";
+
+    let upEl = document.createElement("button");
+    if (taskIdx ===0){
+        upEl.disabled = true;
+    }
+    upEl.innerText = "⇡";
+    upEl.onclick = () => {
+        ///
+    };
+    ctrlbarEl.append(upEl);
+
+
+    let downEl = document.createElement("button");
+    downEl.innerText = "⇣";
+    downEl.onclick = () => {
+        ///
+    };
+    ctrlbarEl.append(downEl);
+
+    let cancelEl = document.createElement("button");
+    cancelEl.innerText = "x";
+    cancelEl.onclick = () => {
+        tasks.splice(taskIdx, 1);
+        renderTaskItems();
+    };
+
+    ctrlbarEl.append(cancelEl);
+
+    return ctrlbarEl;
+
+
 }
 
 renderEditor();
